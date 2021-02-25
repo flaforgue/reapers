ecfd488f1756b0034fdbe2234a39cca70de31eae
flaforgue <florian.laforgue@neoxia.com>
2021-02-26 00:26:13 +0100

    WIP

--- packages/client/src/components/game/game/game.tsx
+++ packages/client/src/components/game/game/game.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import styles from './game.module.scss';
-import * as BABYLON from 'babylonjs';
-import 'babylonjs-loaders';
+import { Player, World } from '../../../models';
 
 const Game: React.FC = () => {
   const canvasEl = useRef<HTMLCanvasElement>(null);
@@ -12,47 +11,25 @@ const Game: React.FC = () => {
 
   useEffect(() => {
     if (canvasEl.current) {
-      const handleWindowResize = (): void => engine.resize();
-      window.addEventListener('resize', handleWindowResize);
-
       const canvas = canvasEl.current;
-      const engine = new BABYLON.Engine(canvas, true);
-      const scene = new BABYLON.Scene(engine);
-      const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
+      const world = new World(canvas);
+      const player = new Player(world.scene);
 
-      // Camera
-      const camera = new BABYLON.ArcRotateCamera(
-        'camera',
-        -Math.PI / 2,
-        Math.PI / 2.5,
-        3,
-        new BABYLON.Vector3(0, 0, 0),
-        scene,
-      );
-      camera.attachControl(canvas, true);
+      const handleWindowResize = (): void => world.engine.resize();
+      const handleFocus = (): void => {
+        console.log('handleBodyFocus');
+        canvasEl.current?.focus();
+      };
+      window.addEventListener('resize', handleWindowResize);
+      document.addEventListener('focus', handleFocus);
 
-      // Music
-      // const music = new BABYLON.Sound('cello', '/sounds/battle.wav', scene, null, {
-      //   loop: true,
-      //   autoplay: true,
-      // });
-
-      // Ground
-      const ground = BABYLON.MeshBuilder.CreateGround('ground', { width: 10, height: 10 });
-      const groundMat = new BABYLON.StandardMaterial('roofMat', scene);
-      groundMat.diffuseTexture = new BABYLON.Texture('/textures/ground.png', scene);
-      ground.material = groundMat;
-
-      // Model
-      BABYLON.SceneLoader.ImportMeshAsync('', '/models/', 'spider.glb');
-
-      // Rendering
-      engine.runRenderLoop(function () {
-        scene.render();
+      world.engine.runRenderLoop(function () {
+        world.scene.render();
       });
 
       return (): void => {
         window.removeEventListener('resize', handleWindowResize);
+        document.removeEventListener('focus', handleFocus);
       };
     }
   }, [canvasEl.current]);
---
+++ packages/client/src/models/index.ts
@@ -0,0 +1,4 @@
+import World from './world';
+import Player from './player';
+
+export { World, Player };
